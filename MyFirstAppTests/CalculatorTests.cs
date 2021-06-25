8ae6d17a7b6778594464017d8d9e655ec927e2bb
Marc Milà <milamartosmarc5@gmail.com>
2021-06-25 10:48:41 +0200

    Initial commit with calculator example.

---
+++ MyFirstAppTests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Add(2, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void SubstractTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Substract(2, 2) == 0);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Multiply(2, 3) == 6); 
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Divide(4, 1) == 4); ;
+        }
+    }
+}
