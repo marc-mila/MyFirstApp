1d5941de815dfa141ea78fa744661d5f9e5fd491
Marc Milà <milamartosmarc5@gmail.com>
2021-07-01 14:11:27 +0200

    Abstract implementation. Closes #13

---
+++ MyFirstApp/AbstractClasses/GeeksForGeeks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.AbstractClasses
+{
+    public abstract class GeeksForGeeks : ICloneable
+    {
+        /*public object Clone()
+        {
+            throw new NotImplementedException();
+        }*/
+
+        public abstract object Clone();
+        public abstract void gfg();
+
+        //sobreescribible
+        public virtual string VirtualMethod()
+        {
+            return "we are in VirtualMethod";
+        }
+    
+        public string Test()
+        {
+            return "we are testing";
+        }
+    }
+}
